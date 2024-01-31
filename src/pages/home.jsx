533653dff0d6d00b86885c7a2787fb8769c7a35d
mustofa-k <kamalmustofa241@gmail.com>
2024-01-31 15:55:11 +0700

    add button wa

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import About from "../components/about/About";
 import Hero from "../components/hero/Hero";
 import Navbars from "../components/navbar/Navbars";
@@ -5,13 +7,37 @@ import Services from "../components/services/Services";
 import Team from "../components/team/Team";
 
 function Home() {
+  const whatsappMessage = encodeURIComponent("Hallo, saya ingin menggunakan jasa pembuatan website.");
+
   return (
     <>
-      <Navbars />
-      <Hero />
-      <Services />
-      <About />
-      <Team />
+      <div>
+        <Navbars />
+        <Hero />
+        <Services />
+        <About />
+        <Team />
+      </div>
+      <div style={{ position: "fixed", bottom: 20, right: 20, zIndex: 999 }}>
+        <a
+          href={`https://wa.me/+6281292793863?text=${whatsappMessage}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{
+            backgroundColor: "#25D366",
+            color: "#fff",
+            padding: "10px 20px",
+            borderRadius: 5,
+            textDecoration: "none",
+            boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <FontAwesomeIcon icon={faWhatsapp} style={{ marginRight: 10 }} />
+          Contact via WhatsApp
+        </a>
+      </div>
     </>
   );
 }
