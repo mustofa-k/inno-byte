ebcece3c6a7a586394998aeef4fd829d05d9a849
maulanaabdullana2 <maulanaabdullana123@gmail.com>
2024-02-01 18:14:11 +0700

    add button read more

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,14 +1,17 @@
-import { Container, Card, Button, Row, Col } from "react-bootstrap";
+import { Container, Card, Row, Col, Image } from "react-bootstrap";
 import project1 from "../../assets/project1.png";
 import project2 from "../../assets/project2.png";
 import project3 from "../../assets/project3.png";
-import "./project.css"
+import vector from "../../assets/Vector.png";
+import "./project.css";
 
 function Project() {
   return (
     <div className="mt-5">
       <Container>
-        <h1 style={{ color: "#000000" }} className="tes">More Projects</h1>
+        <h1 style={{ color: "#000000" }} className="tes">
+          More Projects
+        </h1>
         <Row className="mt-5">
           <Col lg="4" md="6" sm="12">
             <Card style={{ width: "100%", marginTop: "20px" }}>
@@ -23,7 +26,21 @@ function Project() {
                   Some quick example text to build on the card title and make up
                   the bulk of the cards content.
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <div
+                  style={{
+                    borderBottom: "3px solid #0099FF",
+                    display: "inline-block",
+                    paddingBottom: "5px",
+                  }}
+                >
+                  <a
+                    href="#"
+                    style={{ textDecoration: "none", color: "#333333" }}
+                  >
+                    Read More
+                    <Image src={vector} style={{ marginLeft: "5px" }} />
+                  </a>
+                </div>
               </Card.Body>
             </Card>
           </Col>
@@ -38,7 +55,21 @@ function Project() {
                   Some quick example text to build on the card title and make up
                   the bulk of the cards content.
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <div
+                  style={{
+                    borderBottom: "3px solid #0099FF",
+                    display: "inline-block",
+                    paddingBottom: "5px",
+                  }}
+                >
+                  <a
+                    href="#"
+                    style={{ textDecoration: "none", color: "#333333" }}
+                  >
+                    Read More
+                    <Image src={vector} style={{ marginLeft: "5px" }} />
+                  </a>
+                </div>
               </Card.Body>
             </Card>
           </Col>
@@ -55,7 +86,21 @@ function Project() {
                   Some quick example text to build on the card title and make up
                   the bulk of the cards content.
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <div
+                  style={{
+                    borderBottom: "3px solid #0099FF",
+                    display: "inline-block",
+                    paddingBottom: "5px",
+                  }}
+                >
+                  <a
+                    href="#"
+                    style={{ textDecoration: "none", color: "#333333" }}
+                  >
+                    Read More
+                    <Image src={vector} style={{ marginLeft: "5px" }} />
+                  </a>
+                </div>
               </Card.Body>
             </Card>
           </Col>
