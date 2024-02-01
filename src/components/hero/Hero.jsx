50d4b001e4031dc2202e5e5d17188595b1b34c72
maulanaabdullana2 <maulanaabdullana123@gmail.com>
2024-02-01 16:08:28 +0700

    fix rp

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -8,12 +8,18 @@ function Hero() {
       <Container>
         <Row>
           <Col lg={6} sm={6} style={{ marginTop: "10%" }}>
-            <h1 className="custom-heading">Solusi Website Premium dengan Kualitas Terbaik!</h1>
+            <h1 className="custom-heading">
+              Solusi Website Premium dengan Kualitas Terbaik!
+            </h1>
             <div className="d-flex mt-2">
-              <h1>Mulai dari Harga</h1>
-              <div className="harga ms-3">Rp 600.000</div>
+              <h1>
+                Mulai dari Harga <p className="harga">Rp 600.000</p>
+              </h1>
             </div>
-            <p className="mt-1 w-75">Berkarya dengan Gaya, Bangun Online Presence Anda yang Luar Biasa! Jangan Ketinggalan, Wujudkan Website Impian Anda Sekarang</p>
+            <p className="mt-1 w-75">
+              Berkarya dengan Gaya, Bangun Online Presence Anda yang Luar Biasa!
+              Jangan Ketinggalan, Wujudkan Website Impian Anda Sekarang
+            </p>
 
             <button className="button" style={{ padding: "10px 30px" }}>
               Consultasi sekarang
@@ -31,7 +37,10 @@ function Hero() {
             </button> */}
           </Col>
           <Col lg={6} sm={6} style={{ marginTop: "5%" }}>
-            <Image src={homeicon} style={{ maxWidth: "100%", maxHeight: "100%" }} />
+            <Image
+              src={homeicon}
+              style={{ maxWidth: "100%", maxHeight: "100%" }}
+            />
           </Col>
         </Row>
       </Container>
