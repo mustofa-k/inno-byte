72a7f9020e62d9f47a8a1239e6938ba15c01fa7b
maulanaabdullana2 <maulanaabdullana123@gmail.com>
2024-02-05 18:01:48 +0700

    add button read more

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -7,7 +7,7 @@ import layanan from "../../assets/layanan.png";
 
 function Services() {
   return (
-    <div className="services-container">
+    <div>
       <div className="services container-fluid pb-5">
         <div className="content container text-center mt-5">
           <div className="header">
@@ -16,12 +16,18 @@ function Services() {
         </div>
         <div className="container mt-5">
           <Row className="mt-5">
-            <Col xs={12} md={6} lg={3}>
-              <div className="cart bg-white text-center p-4 d-flex flex-column align-items-center justify-content-center">
-                <div className="img-services mb-3">
-                  <img src={speed} alt="Speed" />
+            <Col
+              md={6}
+              lg={3}
+              className="mt-2 mt-md-0 mt-lg-0 mt-xl-0 mt-xxl-0"
+            >
+              <div className="cart bg-white text-center p-2">
+                <div className="d-flex justify-content-center">
+                  <div className="img-services mt-4">
+                    <img src={speed} alt="" />
+                  </div>
                 </div>
-                <p className="mt-3 title">Cepat</p>
+                <p className="mt-5 title">Cepat</p>
                 <p className="mt-3 desc">
                   Sudah tidak berlaku lagi membuat website dengan waktu yang
                   lama, Inno-byte akan mengerjakan dengan cepat website atau
@@ -29,23 +35,35 @@ function Services() {
                 </p>
               </div>
             </Col>
-            <Col xs={12} md={6} lg={3}>
-              <div className="cart bg-white text-center p-4 d-flex flex-column align-items-center justify-content-center">
-                <div className="img-services mb-3">
-                  <img src={mudah} alt="Mudah" />
+            <Col
+              md={6}
+              lg={3}
+              className="mt-2 mt-md-0 mt-lg-0 mt-xl-0 mt-xxl-0"
+            >
+              <div className="cart bg-white text-center p-2">
+                <div className="d-flex justify-content-center">
+                  <div className="img-services mt-4">
+                    <img src={mudah} alt="" />
+                  </div>
                 </div>
-                <p className="mt-3 title">Murah</p>
+                <p className="mt-5 title">Murah</p>
                 <p className="mt-3 desc">
                   kamu bisa membuat website kamu mulai dari Rp.600.000
                 </p>
               </div>
             </Col>
-            <Col xs={12} md={6} lg={3}>
-              <div className="cart bg-white text-center p-4 d-flex flex-column align-items-center justify-content-center">
-                <div className="img-services mb-3">
-                  <img src={templates} alt="Custom Tampilan" />
+            <Col
+              md={6}
+              lg={3}
+              className="mt-2 mt-md-0 mt-lg-0 mt-xl-0 mt-xxl-0"
+            >
+              <div className="cart bg-white text-center p-2">
+                <div className="d-flex justify-content-center">
+                  <div className="img-services mt-4">
+                    <img src={templates} alt="" />
+                  </div>
                 </div>
-                <p className="mt-3 title">Custom Tampilan</p>
+                <p className="mt-5 title">Custom Tampilan</p>
                 <p className="mt-3 desc">
                   Bosan dengan template yang sama atau tampilan gitu gitu aja?
                   Tenang, Inno-byte bisa custom tampilan sesuai dengan kebutuhan
@@ -53,12 +71,18 @@ function Services() {
                 </p>
               </div>
             </Col>
-            <Col xs={12} md={6} lg={3}>
-              <div className="cart bg-white text-center p-4 d-flex flex-column align-items-center justify-content-center">
-                <div className="img-services mb-3">
-                  <img src={layanan} alt="Layanan 24/7" />
+            <Col
+              md={6}
+              lg={3}
+              className="mt-2 mt-md-0 mt-lg-0 mt-xl-0 mt-xxl-0"
+            >
+              <div className="cart bg-white text-center p-2">
+                <div className="d-flex justify-content-center">
+                  <div className="img-services mt-4">
+                    <img src={layanan} alt="" />
+                  </div>
                 </div>
-                <p className="mt-3 title">Layanan 24/7</p>
+                <p className="mt-5 title">Layanan 24/7</p>
                 <p className="mt-3 desc">
                   kamu bisa consultasi sebelum memesan website atau aplikasi
                   kamu
